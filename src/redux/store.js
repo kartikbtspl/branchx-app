bf1c49d277fb20fb16ee2fc0be0738d929ebe04a
kartikbtspl <kartik@brilliantechsoft.com>
2025-06-04 18:23:18 +0530

    did some changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import campaignReducer from './slices/campaignSlice'
+import loginReducer from './slices/authSlice'
 
 export const store = configureStore({
   reducer: {
     campaign: campaignReducer,
+    auth: loginReducer, 
   },
 })
